e345d9782e534f19a6fea98cecbc9d144ec2992d
Simone Cesano <simonecesano@libero.it>
2022-09-04 17:29:18 +0200

    Style key order is now preserved

--- lib/Mojo/DOM/Role/Style.pm
+++ lib/Mojo/DOM/Role/Style.pm
@@ -7,34 +7,73 @@ package Mojo::DOM::Role::Style;
 
 use Mojo::Base -role;
 use Want;
+use List::Util qw/uniq/;
+
+use Mojo::Util qw/dumper/;
 
 sub style {
     my $self = shift;
+
     my $css = $self->attr('style');
-    my $h = $css ? { split /\;|\:/, $css } : {};
 
-    # print 'OBJECT' if want('OBJECT');
-    # print 'HASH'   if want('HASH');
+    my ($h, $k) = _from_css($css);
 
-    if (scalar @_ == 2) {
-	$h->{$_[0]} = $_[1];
+    # print $css;
+    # print dumper $k;
 
-	my $css = join ';', map { join ':', $_, $h->{$_} } keys %{$h};
-
-	$self->attr('style', $css);
-
-	return want('OBJECT') ? $self :
-	    want('HASH') ? $h :
-	    $css;
-    } elsif (scalar @_ == 1) {
+    if (scalar @_ == 1 && ! ref $_[0]) {
 	return $h->{shift()}
-    } elsif (want('OBJECT')) {
+    }
+
+
+    if ((scalar @_ % 2) == 0 && (scalar @_)) {
+	my $m = { @_ };
+	$css = _to_css($m, []);
+	$self->attr('style', $css);
+    } elsif (ref $_[0] eq 'HASH') {
+	# merge
+	for (keys %{$_[0]}) { $h->{$_} = $_[0]->{$_} }
+	$css = _to_css($h, $k);
+	$self->attr('style', $css);
+    }
+
+
+    if (want('OBJECT')) {
 	return $self;
     } elsif (want('HASH')) {
 	return $h;
     } else {
 	return $css;
     }
+}
+
+
+
+# my $query = $url->query;
+# $url      = $url->query({merge => 'to'});
+# $url      = $url->query([append => 'with']);
+# $url      = $url->query(replace => 'with');
+# $url      = $url->query('a=1&b=2');
+# $url      = $url->query(Mojo::Parameters->new);
+
+sub _to_css {
+    my $h = shift;
+    my $k = shift || [];
+
+    $k = [ uniq (@$k, keys %$h) ];
+
+    my $css = join ';', map { join ':', $_, $h->{$_} } @$k;
+    return $css
+}
+
+sub _from_css {
+    my $css = shift;
+    unless ($css) { return wantarray ? ({}, []) : {} }
+
+    my $k = [ map { /^(.+?)\s*:/; $1 } split /\s*;\s*/, $css ];
+    my $h = { map { split /\s*:\s*/, $_, 2 } split /\s*;\s*/, $css };
+
+    return wantarray ? ($h, $k) : $h
 }
 
 1;
